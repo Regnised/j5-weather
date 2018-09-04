cf66c10493136224581db2714c9638caf374547f
Vasilii Onoprienko <vaonop@pascalium.com>
2018-09-04 17:27:59 +0300

    Refactored

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const five = require("johnny-five");
 const board = new five.Board();
-const http = require('http');
 const weather = require('openweather-apis');
 weather.setLang('en');
-weather.setCityId(693457);
 weather.setAPPID('2164ddc779ae96e8f10e98d3c9982f2a');
 
-board.on("ready", function () {
+board.on("ready", async function () {
     const lcd = new five.LCD({
         controller: "PCF8574"
     });
+    let data, Smila, Cherkassy;
+    let flag = 0;
 
-
-    weather.getAllWeather((err , JSONObj) => {
-        lcd.print("Smila," + JSONObj.weather[0].main + "T:" + JSONObj.main.temp);
-        lcd.cursor(1, 0).print("Wind:"+ JSONObj.wind.speed + "P:"+JSONObj.main.pressure + "H:"+ JSONObj.main.humidity);
-    });
-
-    http.createServer((req, res) => {
-        res.setHeader('Content-Type', 'text/html');
-        res.setHeader('X-Foo', 'bar');
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('ok');
-    }).listen(3000);
+    setInterval(async function() {
+        Smila = await getWeather(693457);
+        Cherkassy = await getWeather(710791);
+        data = [Smila, Cherkassy];
+        lcd.clear();
+        lcd.cursor(0, 0).print(data[flag][0]);
+        lcd.cursor(1, 0).print(data[flag][1]);
+        flag = (flag) ? 0 : 1;
+    }, 5000);
 });
+
+function getWeather(id) {
+    return new Promise((resolve, reject) => {
+        weather.setCityId(id);
+        weather.getAllWeather((err, JSONObj) => {
+            if (err) return reject('API error');
+            resolve([ JSONObj.name + " " + JSONObj.weather[0].main, "Temp:" + parseInt(JSONObj.main.temp) + " Wind:" + parseInt(JSONObj.wind.speed)]);
+        });
+    });
+}
